1ae349dbe7e374dada42b50f3b8360ec194fbe97
Jeeseob <jeeseob5761@gmail.com>
2022-10-13 16:11:13 +0900

    ADD: EKS Manager-cluster make script 추가

--- EKS-Manager/init.sh
+++ EKS-Manager/init.sh
@@ -3,13 +3,18 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 
+sudo apt-get install -y amazon-efs-utils
+
+ehco "$ makeCluster after setting 'aws configure'"
---
+++ EKS-Manager/makeCluster.sh
@@ -0,0 +1,13 @@
+eksctl create cluster \
+    --name groom-eks\
+    --region ap-northeast-2 \
+    --with-oidc \
+    --ssh-access \
+    --ssh-public-key groom-EKS \
+    --nodes 3 \
+    --node-type t3.medium \
+    --node-volume-size=20 \
+    --managed
+
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" >> ~/.bashrc
