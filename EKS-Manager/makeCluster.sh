dfa9e80d26e9fb2e318d5cc52e41448620e52ff5
Jeeseob Kim <81093419+Jeeseob@users.noreply.github.com>
2022-10-14 12:00:03 +0900

    Update makeCluster.sh

--- EKS-Manager/makeCluster.sh
+++ EKS-Manager/makeCluster.sh
@@ -9,8 +9,3 @@ eksctl create cluster \
     --node-type t3.medium \
     --node-volume-size=20 \
     --managed
-
-wait
-# 2. kubectl 자동완성 설정 
-source <(kubectl completion bash)
-echo "source <(kubectl completion bash)" >> ~/.bashrc
